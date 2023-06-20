b212858a5e9e6d418cdc1003489a339a55b2a679
Artemova <artemovaok84@mail.ru>
2023-06-20 17:16:46 +0300

    Test without mistake

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -14,7 +14,7 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("goodbye"));
+                .body("data", equalTo("Hello"));
     }
 
 }
